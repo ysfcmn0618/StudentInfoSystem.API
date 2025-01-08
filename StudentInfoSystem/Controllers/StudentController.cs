f477d08811235ad51219ccf260c4af9255ed9a90
yusuf <yusufcimen90@windowslive.com>
2025-01-08 14:37:18 +0300

    Add project files.

---
+++ StudentInfoSystem/Controllers/StudentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentInfoSystem.Models;
+
+namespace StudentInfoSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public static List<Student> Students=new List<Student>();
+
+    }
+}
---
+++ StudentInfoSystem/Models/Student.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace StudentInfoSystem.Models
+{
+    public class Student
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+        public int StudentId { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        public string Gender { get; set; }
+        [Required]
+        [MaxLength(300)]
+        public string Address { get; set; }
+        [Required]
+        public DateTime EnrollmentDate { get; set; }
+        public string GradeLevel { get; set; }
+        public float GPA { get; set; }
+        public List<string> Courses { get; set; } = new List<string>();
+        public bool IsActive { get; set; }
+        public string ParentName { get; set; }
+        public string ParentContact { get; set; }
+        public string PhotoUrl { get; set; }
+        public Student()
+        {
+            EnrollmentDate = DateTime.Now; // Varsayılan olarak şu anki tarihi ayarlar.
+        }
+    }
+}
