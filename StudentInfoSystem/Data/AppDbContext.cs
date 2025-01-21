baea4d31739ccfbfe9faa7f402beeb282ee1bca8
yusuf <yusufcimen90@windowslive.com>
2025-01-21 19:30:57 +0300

    Databaseimizin sanal görünütüsü oluşturuldu connection string appsetting.json dosyasına eklendi

---
+++ StudentInfoSystem/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentInfoSystem.Data.Entities;
+
+namespace StudentInfoSystem.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) 
+        {
+            
+        }
+        public DbSet<StudentEntity> Students { get; set; }
+        public DbSet<ContactEntity> Contacts { get; set; }
+        public DbSet<LessonEntity> Lessons { get; set; }
+    }
+}
